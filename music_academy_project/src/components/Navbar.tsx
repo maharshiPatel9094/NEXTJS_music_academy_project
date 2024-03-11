6db0ca25482fa3651debbfb28e4582847e1fcf8a
maharshiPatel9094 <144848033+maharshiPatel9094@users.noreply.github.com>
2024-03-11 13:06:57 -0400

    menu design from acertainity ui

--- music_academy_project/src/components/Navbar.tsx
+++ music_academy_project/src/components/Navbar.tsx
@@ -1,10 +1,34 @@
-import React from 'react'
+// we have to write this in order to use the react component
+"use client";
+import React, { useState } from "react";
+import {
+  HoveredLink,
+  Menu,
+  MenuItem,
+  ProductItem,
+} from "@/components/ui/navbar-menu";
+import { cn } from "@/utils/cn";
+import Link from "next/link";
 
-
-function Navbar() {
-    return (
-        <div>NavBar</div>
-    );
+function Navbar({ className }: { className?: string }) {
+  const [active, setActive] = useState<string | null>(null);
+  return (
+    <div
+      className={cn("fixed top-10 inset-x-0 max-w-2xl mx-auto z-50", className)}
+    >
+      <Menu setActive={setActive}>
+        <Link href={"#"}>
+          <MenuItem
+            setActive={setActive}
+            active={active}
+            item="Home"
+          >
+            
+          </MenuItem>
+        </Link>
+      </Menu>
+    </div>
+  );
 }
 
-export default Navbar
+export default Navbar;
