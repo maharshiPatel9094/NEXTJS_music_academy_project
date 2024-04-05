98d5205d86fbea2d7f28d02569b879ad9f853511
maharshiPatel9094 <144848033+maharshiPatel9094@users.noreply.github.com>
2024-04-05 11:42:24 -0400

    images added

--- music_academy_project/src/app/page.tsx
+++ music_academy_project/src/app/page.tsx
@@ -1,4 +1,5 @@
 import FeaturedCourses from "@/components/FeaturedCourses";
+import Footer from "@/components/Footer";
 import HeroSection from "@/components/HeroSection";
 import Instructors from "@/components/Instructors";
 import MusicSchoolTestimonial from "@/components/TestimonialCards";
@@ -17,8 +18,9 @@ export default function Home() {
       <MusicSchoolTestimonial/>
       <UpcomingWebinars/>
       <Instructors />
+      <Footer />
 
-      
+
     </main>
   );
 }
